02c4c0fa0e8785853eda28fe027eebd35b595efa
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-10-12 22:17:44 +0900

    header

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,13 @@
 $(function(){
     $('header').animate({top:"0"},1000);
+
+    $(window).on('scroll', function(){
+        var windowScroll = $(this).scrollTop();
+
+        if (windowScroll > 30){
+            $('header').css({'background-color' : 'rgba(0, 0, 0, 0.6)'});
+        } else {
+            $('header').css({'background-color' : 'transparent'}); 
+        }
+    });
 });
